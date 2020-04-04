5215960e1634bb6bc5e4fbd7fa5dce9e9d685db6
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-04 19:18:26 +0300

    add main file and write the documentation

---
+++ multiplication_table/main.c
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: main.c
+Author: Liri (4/4/2020)
+Purpose: This file print the multiplication table
+		 from INITIAL_NUMBER to FINAL_NUMBER
+\********************************************************/
