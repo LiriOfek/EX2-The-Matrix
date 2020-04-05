627a105428318a2f8e2be698d03e3e0ae36fb8e8
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-05 10:29:52 +0300

    add main file with documentation

---
+++ chess_table/main.c
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: main.c
+Author: Liri (5/4/2020)
+Purpose: This file print the chess table of given character
+		 and in given size
+\********************************************************/
