200b11799b485ac6c42493967a0f53b8e63734e8
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-05 10:41:47 +0300

    add main function that get number and character and print run the functions that print the chess table

--- chess_table/main.c
+++ chess_table/main.c
@@ -16,6 +16,7 @@ Purpose: This file print the chess table of given character
 #define EVEN_NUMBER (2)
 #define SPACE_STRING (" ")
 #define PRINT_CHARACTER ("%c")
+#define SPACE_CHAR (' ')
 
 int get_number_from_user() {
 	/********************************************************\
@@ -45,41 +46,8 @@ int get_number_from_user() {
 	return number;
 }
 
-void print_chess_table(int size_of_table,
-	char character) {
-	/********************************************************\
-	* Function name - print_chess_table
-	*
-	* Function Purpose - print the chess table of the characters
-	*					 and spaces in size of size_of_table
-	*
-	* Parameters - IN int size_of_table - the size of the table
-	*			   IN char character - the character that will be printed
-	*
-	* Return Value - there isn't return value
-	*
-	* Side Effects - this function has no side effects
-	*
-	* Semantics - this function print the chess table of the characters
-	*			  and spaces in size of size_of_table
-	*
-	* Author - Liri
-	\********************************************************/
-	int row = INITIAL_INDEX; /*the index of the row of the table*/
-	int column = INITIAL_INDEX; /*the index of the column of the table*/
-
-	for (row = INITIAL_INDEX; row <= size_of_table; row++)
-	{
-		for (column = INITIAL_INDEX; column <= size_of_table; column++)
-		{
-			print_character_or_space(row, column, character);
-		}
-		printf(ENTER_STRING);
-	}
-}
-
-void print_character_or_space(int row,
-								int column,
+void print_character_or_space(int row, 
+								int column, 
 								char character) {
 	/********************************************************\
 	* Function name - print_character_or_space
@@ -122,4 +90,75 @@ void print_character_or_space(int row,
 			printf(SPACE_STRING);
 		}
 	}
+}
+
+void print_chess_table(int size_of_table, 
+						char character) {
+	/********************************************************\
+	* Function name - print_chess_table
+	*
+	* Function Purpose - print the chess table of the characters
+	*					 and spaces in size of size_of_table
+	*
+	* Parameters - IN int size_of_table - the size of the table
+	*			   IN char character - the character that will be printed
+	*
+	* Return Value - there isn't return value
+	*
+	* Side Effects - this function has no side effects
+	*
+	* Semantics - this function print the chess table of the characters
+	*			  and spaces in size of size_of_table
+	*
+	* Author - Liri
+	\********************************************************/
+	int row = INITIAL_INDEX; /*the index of the row of the table*/
+	int column = INITIAL_INDEX; /*the index of the column of the table*/
+
+	for (row = INITIAL_INDEX; row <= size_of_table; row++)
+	{
+		for (column = INITIAL_INDEX; column <= size_of_table; column++)
+		{
+			print_character_or_space(row, 
+										column, 
+										character);
+		}
+		printf(ENTER_STRING);
+	}
+}
+
+void main() {
+	/********************************************************\
+	* Function name - main
+	*
+	* Function Purpose - get number and character from the user and
+	*					 run the function that print the chess table
+	*					 of in the size of the number the user entered
+	*					 and with the character the user enterd
+	*
+	*
+	* Parameters - no Input/Output parameters
+	*
+	* Return Value - there isn't return value
+	*
+	* Side Effects - this function has no side effects
+	*
+	* Semantics - get number and character from the user and
+	*			  run the function that print the chess table
+	*			  of in the size of the number the user entered
+	*			  and with the character the user enterd
+	*
+	* Author - Liri
+	\********************************************************/
+	int number_from_user = INITIAL_NUMBER; /*the number that the user enter*/
+	char character_from_user = SPACE_CHAR; /*the character that the user enter*/
+
+										   /*get number and character from user*/
+	printf("Enter number: ");
+	number_from_user = get_number_from_user();
+	printf("Enter character: ");
+	character_from_user = getchar();
+
+	print_chess_table(number_from_user, 
+						character_from_user);
 }
