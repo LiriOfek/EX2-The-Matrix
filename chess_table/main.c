cc935b995ef0a357ba15f39d9dec04fcb965c896
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-05 10:32:41 +0300

    add function that get number from user

--- chess_table/main.c
+++ chess_table/main.c
@@ -4,3 +4,39 @@ Author: Liri (5/4/2020)
 Purpose: This file print the chess table of given character
 		 and in given size
 \********************************************************/
+
+#include <stdio.h>
+
+#define INITIAL_NUMBER (0)
+#define ENTER_CHAR ('\n')
+#define DECIMAL (10)
+#define CHAR_ZERO ('0')
+
+
+int get_number_from_user() {
+	/********************************************************\
+	* Function name - get_number_from_user
+	*
+	* Function Purpose - get the number that the user type
+	*
+	* Parameters - OUT integer number - the number that the user type
+	*
+	* Return Value - the number that the user type
+	*
+	* Side Effects - this function has no side effects
+	*
+	* Semantics - this function get the number that the user types
+	*			  and return it.
+	*
+	* Author - Liri
+	\********************************************************/
+	int number = INITIAL_NUMBER; /*the total number the user enter*/
+	int char_from_user; /*the next char from user*/
+
+	while ((char_from_user = getchar()) != ENTER_CHAR) {
+		/*multiply the digits seen already by 10 and
+		add the decimal value of the new digit*/
+		number = number * DECIMAL + (char_from_user - CHAR_ZERO);
+	}
+	return number;
+}
